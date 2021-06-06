43cf259a58666026aa8ef3527a56afe70c020c34
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-06-06 01:28:48 -0500

    index.js was called as a module and so protecting its content

--- assets/index.js
+++ assets/index.js
@@ -30,10 +30,8 @@ class MediaPlayer {
     }
 }
 
-{
 const video = document.querySelector('video')
 const button = document.querySelector('button')
 const player = new MediaPlayer({el: video}) //as a dictionary
 
 button.onclick = () => player.toggle()
-}
