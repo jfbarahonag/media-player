f88f7e71aa508d361fd34299e97a0332f3015681
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-06-14 12:10:19 -0500

    Call, Apply, Bind section was added

--- assets/index.js
+++ assets/index.js
@@ -6,7 +6,7 @@ const play = document.querySelector('.play')
 const mute = document.querySelector('.mute')
 const player = new MediaPlayer({
     el: video, 
-    plugins: [new AutoPlay()]
+    plugins: [/*new AutoPlay()*/]
 }) //as a dictionary
 
 play.onclick = () => player.togglePlay()
